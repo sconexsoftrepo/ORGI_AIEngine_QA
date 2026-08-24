60473e9a97551a869834da2f15349195dc98c29d
Thanushya-work <thanushyawork@gmail.com>
2026-08-24 17:43:47 +0530

    Updated SOVI AI Engine code

--- test.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-
-final = AudioSegment.empty()
-
-for i in range(1, 10):
-    final += AudioSegment.from_mp3(f"{i}.mp3")
-
-final.export("final_quiz_intro.mp3", format="mp3")
